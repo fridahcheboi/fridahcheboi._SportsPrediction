84ec63988901ca7ff9ee92b6aa3dd31634e2b402
FRIDAH CHEBOI <fridah.cheboi@gmail.com>
2024-06-23 20:50:59 +0000

    Update app2.py

--- app2.py
+++ app2.py
@@ -52,21 +52,21 @@ def main():
     )
 
     # Input features
-    movement_reactions = st.number_input('Movement Reactions', min_value=0, max_value=100, value=50, key='movement_reactions')
-    mentality_composure = st.number_input('Mentality Composure', min_value=0, max_value=100, value=50, key='mentality_composure')
-    potential = st.number_input('Potential', min_value=0, max_value=100, value=50, key='potential')
-    lf = st.number_input('LF', min_value=0, max_value=100, value=50, key='lf')
-    cf = st.number_input('CF', min_value=0, max_value=100, value=50, key='cf')
-    rf = st.number_input('RF', min_value=0, max_value=100, value=50, key='rf')
-    wage_eur = st.number_input('Wage (EUR)', min_value=0, value=0, key='wage_eur')
-    lw = st.number_input('LW', min_value=0, max_value=100, value=50, key='lw')
-    rw = st.number_input('RW', min_value=0, max_value=100, value=50, key='rw')
-    power_shot_power = st.number_input('Power Shot Power', min_value=0, max_value=100, value=50, key='power_shot_power')
-    lwb = st.number_input('LWB', min_value=0, max_value=100, value=50, key='lwb')
-    rwb = st.number_input('RWB', min_value=0, max_value=100, value=50, key='rwb')
-    value_eur = st.number_input('Value (EUR)', min_value=0, value=0, key='value_eur')
-    release_clause_eur = st.number_input('Release Clause (EUR)', min_value=0, value=0, key='release_clause_eur')
-    passing = st.number_input('Passing', min_value=0, max_value=100, value=50, key='passing')
+    movement_reactions = st.number_input('Player Reactions Attribute', min_value=20, max_value=96, value=50, key='movement_reactions')
+    mentality_composure = st.number_input('Player Mentality Composure', min_value=0, max_value=100, value=50, key='mentality_composure')
+    potential = st.number_input('Player Potential Overall Attibute', min_value=0, max_value=100, value=50, key='potential')
+    lf = st.number_input('Player Attribute Playing as LF', min_value=0, max_value=100, value=50, key='lf')
+    cf = st.number_input('Player Attribute Playing as CF', min_value=0, max_value=100, value=50, key='cf')
+    rf = st.number_input('Player Attribute Playing RF', min_value=0, max_value=100, value=50, key='rf')
+    wage_eur = st.number_input('Player Weekly Wage (in EUR)', min_value=0, value=0, key='wage_eur')
+    lw = st.number_input('Player Attribute Playing as LW', min_value=0, max_value=100, value=50, key='lw')
+    rw = st.number_input('Player Attribute Playing RW', min_value=0, max_value=100, value=50, key='rw')
+    power_shot_power = st.number_input('Player Power Shot Power', min_value=0, max_value=100, value=50, key='power_shot_power')
+    lwb = st.number_input('Player Attribute Playing as LWB', min_value=0, max_value=100, value=50, key='lwb')
+    rwb = st.number_input('Player Attribute Playing as RWB', min_value=0, max_value=100, value=50, key='rwb')
+    value_eur = st.number_input('Player Value ( in EUR)', min_value=0, value=0, key='value_eur')
+    release_clause_eur = st.number_input('Player Release Clause (in EUR)', min_value=374, value=5000, key='release_clause_eur')
+    passing = st.number_input('Player Passing Attribute', min_value=20, max_value=93, value=50, key='passing')
 
     if st.button('Overall Rating'):
         input_features = [
@@ -75,7 +75,7 @@ def main():
         ]
         try:
             rf_prediction = predict_rating(rf_model, input_features)
-            st.success(f'Random Forest predicted overall rating: {rf_prediction:.2f}')
+            st.success(f'Random Forest predicted overall rating: {rf_prediction:.2f}\n  Accuracy Score: 97%')
         except Exception as e:
             st.error(f"Error during prediction: {e}")
 
